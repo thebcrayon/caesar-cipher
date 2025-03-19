644ef22475fc3452d7867138c712a34200d44dd5
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2025-03-19 12:36:02 -0500

    Fix rubocop errors, add gem files

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,38 +1,55 @@
+# frozen_string_literal: true
+
 def caesar_string(original_string, shift_number)
   # Need to turn original string into array to get indexes
-  # relative to the alphabet with #get_alphabet. 
-  original_string_array = original_string.split("")
-  original_indexes = original_string_array.map do |char|
-    letter?(char) ? get_alphabet.find_index(char.downcase) + 1 : char
-  end
+  # relative to the alphabet with #alphabet.
+  original_indexes = indexes(original_string_array(original_string))
 
   # Mapping those indexes to move to the right 'shift_number'
   # of digits
-  shifted_indexes = original_indexes.map do |index|
-   index.is_a?(Integer) ? (index + shift_number) % 26 : index
-  end
-  
+  shifted_indexes = shifted_indexes(original_indexes, shift_number)
+
   # Grab the corresponding letter from the newly shifted index.
   # Using Modulo to account for shifts beyond 26 letters
-  new_string_array = shifted_indexes.map do |index|
-    index.is_a?(Integer) ? get_alphabet[index - 1] : index
-  end
+  converted_array = new_string_array(shifted_indexes)
 
   # Check to see what letters are capitalized in original
   # string array and capitalizing the character at the
   # corresponding index of 'new_string_array'
-  new_string_array.map.with_index do |char, idx|
-    char_check = original_string_array[idx]
+  converted_array.map.with_index do |char, idx|
+    char_check = original_string_array(original_string)[idx]
     char_check == char_check.upcase ? char.upcase : char
-  end.join('')
+  end.join
+end
+
+def new_string_array(array)
+  array.map do |index|
+    index.is_a?(Integer) ? alphabet[index - 1] : index
+  end
+end
+
+def indexes(array)
+  array.map do |char|
+    letter?(char) ? alphabet.find_index(char.downcase) + 1 : char
+  end
+end
+
+def shift_indexes(array, shift_number)
+  array.map do |index|
+    index.is_a?(Integer) ? (index + shift_number) % 26 : index
+  end
+end
+
+def original_string_array(str)
+  str.chars
 end
 
 def letter?(char)
   char.match?(/[[:alpha:]]/)
 end
 
-def get_alphabet
-  ('a'..'z').to_a 
+def alphabet
+  ('a'..'z').to_a
 end
 
 my_string = 'What a string!'
